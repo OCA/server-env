86e946cc68082beb6ddcdc99120f99a8c46e2d56
Simone Orsi <simahawk@gmail.com>
2017-11-29 16:51:05 +0100

    [11.0][MIG] mail_environment

--- mail_environment/__manifest__.py
+++ mail_environment/__manifest__.py
@@ -1,19 +1,19 @@
-# -*- coding: utf-8 -*-
-# Copyright 2012-2016 Camptocamp SA
+# Copyright 2012-2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     'name': 'Mail configuration with server_environment',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Tools',
     'summary': 'Configure mail servers with server_environment_files',
-    'author': "Camptocamp,Odoo Community Association (OCA)",
+    'author': "Camptocamp, Odoo Community Association (OCA)",
     'license': 'AGPL-3',
-    'website': 'http://odoo-community.org',
-    'depends': ['fetchmail',
-                'server_environment',
-                ],
-    'data': ['views/fetchmail_server_views.xml',
-             ],
-    'installable': True,
+    'website': 'https://github.com/OCA/server-env',
+    'depends': [
+        'fetchmail',
+        'server_environment',
+    ],
+    'data': [
+        'views/fetchmail_server_views.xml',
+    ],
 }
