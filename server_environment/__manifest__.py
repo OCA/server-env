b4ff859d0df5e10e9a9fa7684d7a2b492277acc0
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2018-06-04 16:15:24 +0200

    [REF] Server Environment: remove dependency on server_environement_files From https://github.com/OCA/server-env/issues/10

--- server_environment/__manifest__.py
+++ server_environment/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "server configuration environment files",
     "version": "11.0.1.0.1",
-    "depends": ["base", "server_environment_files"],
+    "depends": ["base"],
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "summary": "move some configurations out of the database",
     "website": "http://odoo-community.org/",
