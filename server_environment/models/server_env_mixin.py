4f25dc6cc255481b6ba36cd52698769fdd311d7a
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-11-13 11:05:47 +0100

    [IMP] copyright headers
    Use GPL licenses everywhere, to match original code.

--- server_environment/models/server_env_mixin.py
+++ server_environment/models/server_env_mixin.py
@@ -1,5 +1,5 @@
 # Copyright 2018 Camptocamp (https://www.camptocamp.com).
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License GPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import logging
 
