b8b5ad4f000fe0249021c466fad7019c4a4c23b0
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2018-11-15 15:39:30 +0100

    [MIG] fix tests
    Manual forward port of 11.0 migration commit by Thierry Ducrest.

--- server_environment_ir_config_parameter/models/ir_config_parameter.py
+++ server_environment_ir_config_parameter/models/ir_config_parameter.py
@@ -1,4 +1,4 @@
-# Copyright 2016 ACSONE SA/NV
+# Copyright 2016-2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, models, _
@@ -15,7 +15,7 @@ class IrConfigParameter(models.Model):
 
     @api.model
     def get_param(self, key, default=False):
-        value = super(IrConfigParameter, self).get_param(key, default=None)
+        value = super().get_param(key, default=None)
         if serv_config.has_option(SECTION, key):
             cvalue = serv_config.get(SECTION, key)
             if not cvalue:
@@ -39,7 +39,7 @@ class IrConfigParameter(models.Model):
         if serv_config.has_option(SECTION, key):
             # enforce value from config file
             vals = dict(vals, value=serv_config.get(SECTION, key))
-        return super(IrConfigParameter, self).create(vals)
+        return super().create(vals)
 
     @api.multi
     def write(self, vals):
@@ -50,5 +50,5 @@ class IrConfigParameter(models.Model):
                 newvals = dict(vals, value=serv_config.get(SECTION, key))
             else:
                 newvals = vals
-            super(IrConfigParameter, rec).write(newvals)
+            super().write(newvals)
         return True
