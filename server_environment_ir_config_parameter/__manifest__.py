8dd0f59e8a7719fd335d99be1b64e69d77be8d7e
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2018-11-15 15:39:30 +0100

    [MIG] fix tests
    Manual forward port of 11.0 migration commit by Thierry Ducrest.

--- server_environment_ir_config_parameter/__manifest__.py
+++ server_environment_ir_config_parameter/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2016 ACSONE SA/NV
+# Copyright 2016-2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
@@ -7,8 +7,8 @@
         Override System Parameters from server environment file""",
     'version': '12.0.1.0.0',
     'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
-    'website': 'https://odoo-community.org/',
+    'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/server-env/',
     'depends': [
         'server_environment',
     ],
