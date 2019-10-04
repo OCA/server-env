f0223e9a0636cbd9599b2b3d0b1755c5674f8fa1
Florian da Costa <florian.dacosta@akretion.com>
2019-10-04 19:57:19 +0200

    [REF] Adapt method name after change is depency

--- server_environment_data_encryption/__manifest__.py
+++ server_environment_data_encryption/__manifest__.py
@@ -1,7 +1,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Server Environment Data Encryption",
-    "version": "12.0.0.0.1",
+    "version": "12.0.1.0.0",
+    "development_status": 'Alpha',
     "category": "Tools",
     "website": "https://akretion.com/",
     "author": "Akretion, Odoo Community Association (OCA)",
