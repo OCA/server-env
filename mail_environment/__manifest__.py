c62a3b0a6cbb80f49f8eb9ca2f313316a4044a99
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2019-10-10 18:31:51 +0200

    [REF] mail_environment: Black python code

--- mail_environment/__manifest__.py
+++ mail_environment/__manifest__.py
@@ -2,15 +2,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Mail configuration with server_environment',
-    'version': '12.0.1.0.0',
-    'category': 'Tools',
-    'summary': 'Configure mail servers with server_environment_files',
-    'author': "Camptocamp, Odoo Community Association (OCA)",
-    'license': 'AGPL-3',
-    'website': 'https://github.com/OCA/server-env',
-    'depends': [
-        'fetchmail',
-        'server_environment',
-    ],
+    "name": "Mail configuration with server_environment",
+    "version": "12.0.1.0.0",
+    "category": "Tools",
+    "summary": "Configure mail servers with server_environment_files",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/server-env",
+    "depends": ["fetchmail", "server_environment"],
 }
