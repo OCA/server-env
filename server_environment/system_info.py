d099b9269c9433784e22345387590822ddc3672a
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2019-10-12 12:57:26 +0200

    pre-commit, black, isort

--- server_environment/system_info.py
+++ server_environment/system_info.py
@@ -28,38 +28,39 @@ from odoo.tools.config import config
 
 
 def _get_output(cmd):
-    bindir = config['root_path']
-    p = subprocess.Popen(cmd, shell=True, cwd=bindir, stdout=subprocess.PIPE,
-                         stderr=subprocess.STDOUT)
+    bindir = config["root_path"]
+    p = subprocess.Popen(
+        cmd, shell=True, cwd=bindir, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+    )
     return p.communicate()[0].rstrip()
 
 
 def get_server_environment():
     # inspired by server/bin/service/web_services.py
     try:
-        rev_id = 'git:%s' % _get_output('git rev-parse HEAD')
+        rev_id = "git:%s" % _get_output("git rev-parse HEAD")
     except Exception:
         try:
-            rev_id = 'bzr: %s' % _get_output('bzr revision-info')
+            rev_id = "bzr: %s" % _get_output("bzr revision-info")
         except Exception:
-            rev_id = 'Can not retrieve revison from git or bzr'
+            rev_id = "Can not retrieve revison from git or bzr"
 
-    os_lang = '.'.join([x for x in locale.getdefaultlocale() if x])
+    os_lang = ".".join([x for x in locale.getdefaultlocale() if x])
     if not os_lang:
-        os_lang = 'NOT SET'
-    if os.name == 'posix' and platform.system() == 'Linux':
-        lsbinfo = _get_output('lsb_release -a')
+        os_lang = "NOT SET"
+    if os.name == "posix" and platform.system() == "Linux":
+        lsbinfo = _get_output("lsb_release -a")
     else:
-        lsbinfo = 'not lsb compliant'
+        lsbinfo = "not lsb compliant"
     return (
-        ('platform', platform.platform()),
-        ('os.name', os.name),
-        ('lsb_release', lsbinfo),
-        ('release', platform.release()),
-        ('version', platform.version()),
-        ('architecture', platform.architecture()[0]),
-        ('locale', os_lang),
-        ('python', platform.python_version()),
-        ('odoo', release.version),
-        ('revision', rev_id),
+        ("platform", platform.platform()),
+        ("os.name", os.name),
+        ("lsb_release", lsbinfo),
+        ("release", platform.release()),
+        ("version", platform.version()),
+        ("architecture", platform.architecture()[0]),
+        ("locale", os_lang),
+        ("python", platform.python_version()),
+        ("odoo", release.version),
+        ("revision", rev_id),
     )
