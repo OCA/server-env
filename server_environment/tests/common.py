2b78d3e8d3837b3c2db8fab92cde94c8e618b1aa
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2019-10-12 12:57:26 +0200

    pre-commit, black, isort

--- server_environment/tests/common.py
+++ server_environment/tests/common.py
@@ -6,29 +6,27 @@ from contextlib import contextmanager
 from unittest.mock import patch
 
 from odoo.tests import common
-from odoo.addons.server_environment import server_env
 from odoo.tools.config import config
 
-import odoo.addons.server_environment.models.server_env_mixin as \
-    server_env_mixin
+import odoo.addons.server_environment.models.server_env_mixin as server_env_mixin
+from odoo.addons.server_environment import server_env
 
 
 class ServerEnvironmentCase(common.SavepointCase):
-
     def setUp(self):
         super().setUp()
-        self._original_running_env = config.get('running_env')
-        config['running_env'] = 'testing'
+        self._original_running_env = config.get("running_env")
+        config["running_env"] = "testing"
 
     def tearDown(self):
         super().tearDown()
-        config['running_env'] = self._original_running_env
+        config["running_env"] = self._original_running_env
 
     @contextmanager
     def set_config_dir(self, path):
         original_dir = server_env._dir
         if path and not os.path.isabs(path):
-            path = os.path.join(os.path.dirname(__file__,), path)
+            path = os.path.join(os.path.dirname(__file__), path)
         server_env._dir = path
         try:
             yield
@@ -39,18 +37,17 @@ class ServerEnvironmentCase(common.SavepointCase):
     def set_env_variables(self, public=None, secret=None):
         newkeys = {}
         if public:
-            newkeys['SERVER_ENV_CONFIG'] = public
+            newkeys["SERVER_ENV_CONFIG"] = public
         if secret:
-            newkeys['SERVER_ENV_CONFIG_SECRET'] = secret
-        with patch.dict('os.environ', newkeys):
+            newkeys["SERVER_ENV_CONFIG_SECRET"] = secret
+        with patch.dict("os.environ", newkeys):
             yield
 
     @contextmanager
     def load_config(self, public=None, secret=None):
         original_serv_config = server_env_mixin.serv_config
         try:
-            with self.set_config_dir(None), \
-                    self.set_env_variables(public, secret):
+            with self.set_config_dir(None), self.set_env_variables(public, secret):
                 parser = server_env._load_config()
                 server_env_mixin.serv_config = parser
                 yield
--- server_environment/tests/test_environment_variable.py
+++ server_environment/tests/test_environment_variable.py
@@ -3,43 +3,25 @@
 
 
 from odoo.addons.server_environment import server_env
+
 from .common import ServerEnvironmentCase
 
 
 class TestEnvironmentVariables(ServerEnvironmentCase):
-
     def test_env_variables(self):
-        public = (
-            "[section]\n"
-            "foo=bar\n"
-            "bar=baz\n"
-        )
-        secret = (
-            "[section]\n"
-            "bar=foo\n"
-            "alice=bob\n"
-        )
-        with self.set_config_dir(None), \
-                self.set_env_variables(public, secret):
+        public = "[section]\n" "foo=bar\n" "bar=baz\n"
+        secret = "[section]\n" "bar=foo\n" "alice=bob\n"
+        with self.set_config_dir(None), self.set_env_variables(public, secret):
             parser = server_env._load_config()
-            self.assertEqual(
-                list(parser.keys()),
-                ['DEFAULT', 'section']
-            )
+            self.assertEqual(list(parser.keys()), ["DEFAULT", "section"])
             self.assertDictEqual(
-                dict(parser['section'].items()),
-                {'alice': 'bob',
-                 'bar': 'foo',
-                 'foo': 'bar'}
+                dict(parser["section"].items()),
+                {"alice": "bob", "bar": "foo", "foo": "bar"},
             )
 
     def test_env_variables_override(self):
-        public = (
-            "[external_service.ftp]\n"
-            "user=foo\n"
-        )
-        with self.set_config_dir('testfiles'), \
-                self.set_env_variables(public):
+        public = "[external_service.ftp]\n" "user=foo\n"
+        with self.set_config_dir("testfiles"), self.set_env_variables(public):
             parser = server_env._load_config()
-            val = parser.get('external_service.ftp', 'user')
-            self.assertEqual(val, 'foo')
+            val = parser.get("external_service.ftp", "user")
+            self.assertEqual(val, "foo")
