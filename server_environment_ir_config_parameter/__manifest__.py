193f88ddacd2213062b597e00da585f6f467c063
Akim Juillerat <akim.juillerat@camptocamp.com>
2020-01-28 16:32:00 +0100

    [IMP] server_environment_ir_config_parameter: black, isort

--- server_environment_ir_config_parameter/__manifest__.py
+++ server_environment_ir_config_parameter/__manifest__.py
@@ -2,14 +2,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Server Environment Ir Config Parameter',
-    'summary': """
+    "name": "Server Environment Ir Config Parameter",
+    "summary": """
         Override System Parameters from server environment file""",
-    'version': '13.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/server-env/',
-    'depends': [
-        'server_environment',
-    ],
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/server-env/",
+    "depends": ["server_environment"],
 }
