3c89f986da8c2ccc76b1464394041a1ce57ef95e
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-01-28 11:49:20 +0100

    Run pre-commit (black, isort)

--- mail_environment/models/fetchmail_server.py
+++ mail_environment/models/fetchmail_server.py
@@ -2,12 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 import operator
+
 from odoo import api, fields, models
 
 
 class FetchmailServer(models.Model):
     """Incoming POP/IMAP mail server account"""
-    _name = 'fetchmail.server'
+
+    _name = "fetchmail.server"
     _inherit = ["fetchmail.server", "server.env.mixin"]
 
     @property
@@ -26,7 +28,7 @@ class FetchmailServer(models.Model):
         mail_fields.update(base_fields)
         return mail_fields
 
-    server_type = fields.Selection(search='_search_server_type')
+    server_type = fields.Selection(search="_search_server_type")
 
     @api.model
     def _server_env_global_section_name(self):
@@ -34,19 +36,19 @@ class FetchmailServer(models.Model):
 
         Can be customized in your model
         """
-        return 'incoming_mail'
+        return "incoming_mail"
 
     @api.model
     def _search_type(self, oper, value):
         operators = {
-            '=': operator.eq,
-            '!=': operator.ne,
-            'in': operator.contains,
-            'not in': lambda a, b: not operator.contains(a, b),
+            "=": operator.eq,
+            "!=": operator.ne,
+            "in": operator.contains,
+            "not in": lambda a, b: not operator.contains(a, b),
         }
         if oper not in operators:
-            return [('id', 'in', [])]
+            return [("id", "in", [])]
         servers = self.search([]).filtered(
             lambda s: operators[oper](value, s.server_type)
         )
-        return [('id', 'in', servers.ids)]
+        return [("id", "in", servers.ids)]
