5e8cac4a945c434b83dfbad9729b6284125ecf63
Akim Juillerat <akim.juillerat@camptocamp.com>
2020-07-24 11:44:27 +0200

    Create module server_environment_delivery

---
+++ server_environment_delivery/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2020 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+{
+    "name": "Server Environment Delivery",
+    "summary": "Configure prod environment for delivery carriers",
+    "version": "13.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Operations/Inventory/Delivery",
+    "website": "https://github.com/OCA/server-env",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["delivery", "server_environment"],
+}
