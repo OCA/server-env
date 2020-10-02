a6cae6e887efc3344a2a640932f8c2101f9f217c
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2020-10-02 11:21:36 +0200

    [REF] data_encryption: Black python code

--- data_encryption/tests/test_data_encrypt.py
+++ data_encryption/tests/test_data_encrypt.py
@@ -17,83 +17,100 @@ except ImportError as err:  # pragma: no cover
 
 
 class TestDataEncrypted(CommonDataEncrypted):
-
     def test_store_data_no_superuser(self):
         # only superuser can use this model
-        admin = self.env.ref('base.user_admin')
+        admin = self.env.ref("base.user_admin")
         with self.assertRaises(AccessError):
             self.encrypted_data.sudo(admin.id)._encrypted_store(
-                self.crypted_data_name, "My config")
+                self.crypted_data_name, "My config"
+            )
 
     def test_store_data_noenv_set(self):
-        config.pop('running_env', None)
+        config.pop("running_env", None)
         with self.assertRaises(ValidationError):
             self.encrypted_data.sudo()._encrypted_store(
-                self.crypted_data_name, "My config")
+                self.crypted_data_name, "My config"
+            )
 
     def test_store_data_nokey_set(self):
-        config.pop('encryption_key_test', None)
+        config.pop("encryption_key_test", None)
         with self.assertRaises(ValidationError):
             self.encrypted_data.sudo()._encrypted_store(
-                self.crypted_data_name, "My config")
+                self.crypted_data_name, "My config"
+            )
 
     def test_get_data_decrypted_and_cache(self):
         self.encrypted_data.sudo()._encrypted_store(
-            'test_model,1', "My config")
+            "test_model,1", "My config"
+        )
         data = self.encrypted_data.sudo()._encrypted_get(
-            self.crypted_data_name)
+            self.crypted_data_name
+        )
         self.assertEqual(data, "My config")
 
         # Test cache really depends on user (super user) else any user could
         # access the data
-        admin = self.env.ref('base.user_admin')
+        admin = self.env.ref("base.user_admin")
         with self.assertRaises(AccessError):
             self.encrypted_data.sudo(admin)._encrypted_get(
-                self.crypted_data_name)
+                self.crypted_data_name
+            )
 
         # Change value should invalidate cache
         self.encrypted_data.sudo()._encrypted_store(
-            'test_model,1', "Other Config")
+            "test_model,1", "Other Config"
+        )
         new_data = self.encrypted_data.sudo()._encrypted_get(
-            self.crypted_data_name)
+            self.crypted_data_name
+        )
         self.assertEqual(new_data, "Other Config")
 
     def test_get_data_wrong_key(self):
         self.encrypted_data.sudo()._encrypted_store(
-            'test_model,1', "My config")
+            "test_model,1", "My config"
+        )
         new_key = Fernet.generate_key()
-        config['encryption_key_test'] = new_key.decode()
+        config["encryption_key_test"] = new_key.decode()
         with self.assertRaises(ValidationError):
-            self.encrypted_data.sudo()._encrypted_get(
-                self.crypted_data_name)
+            self.encrypted_data.sudo()._encrypted_get(self.crypted_data_name)
 
     def test_get_empty_data(self):
         empty_data = self.encrypted_data.sudo()._encrypted_get(
-            self.crypted_data_name)
+            self.crypted_data_name
+        )
         self.assertEqual(empty_data, None)
 
     def test_get_wrong_json(self):
         self.encrypted_data.sudo()._encrypted_store(
-            self.crypted_data_name, 'config')
+            self.crypted_data_name, "config"
+        )
         with self.assertRaises(ValidationError):
             self.encrypted_data.sudo()._encrypted_read_json(
-                self.crypted_data_name)
+                self.crypted_data_name
+            )
 
     def test_get_good_json(self):
         self.encrypted_data.sudo()._encrypted_store_json(
-            self.crypted_data_name, {'key': 'value'})
+            self.crypted_data_name, {"key": "value"}
+        )
         data = self.encrypted_data.sudo()._encrypted_read_json(
-            self.crypted_data_name)
-        self.assertEqual(data, {'key': 'value'})
+            self.crypted_data_name
+        )
+        self.assertEqual(data, {"key": "value"})
 
     def test_get_empty_json(self):
         data = self.encrypted_data.sudo()._encrypted_read_json(
-            self.crypted_data_name)
+            self.crypted_data_name
+        )
         self.assertEqual(data, {})
 
     def test_get_data_with_bin_size_context(self):
         self.encrypted_data.sudo()._encrypted_store(
-            self.crypted_data_name, "test")
-        data = self.encrypted_data.sudo().with_context(bin_size=True).\
-            _encrypted_get(self.crypted_data_name)
+            self.crypted_data_name, "test"
+        )
+        data = (
+            self.encrypted_data.sudo()
+            .with_context(bin_size=True)
+            ._encrypted_get(self.crypted_data_name)
+        )
         self.assertEqual(data, "test")
