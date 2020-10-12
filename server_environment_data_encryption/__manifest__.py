97f8069c4be66e9d1b2b44f4e93910b7778535ca
Mourad <mourad.elhadj.mimoune@akretion.com>
2020-10-12 15:32:19 +0200

    [MIG] server_environment_data_encryption, data_encryption: Migration to 14.0
    note: exclude tests/fixtures from pre-commit check

--- server_environment_data_encryption/__manifest__.py
+++ server_environment_data_encryption/__manifest__.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Server Environment Data Encryption",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "development_status": "Alpha",
     "category": "Tools",
     "website": "https://github.com/OCA/server-env",
