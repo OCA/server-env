99e3887c29f7fc7753c2a9de4ee8f271697e8e0f
Ivàn Todorovich <ivan.todorovich@gmail.com>
2021-07-05 12:44:39 -0300

    [ADD] payment_environment
    [UPD] Update payment_environment.pot
    
    [UPD] README.rst
    
    [ADD] icon.png

---
+++ payment_environment/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Camptocamp SA
+# @author Iván Todorovich <ivan.todorovich@gmail.com>
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
+
+{
+    "name": "Payment Acquirers' configuration with server_environment",
+    "summary": "Configure payment acquirers with server_environment",
+    "category": "Tools",
+    "version": "13.0.1.0.0",
+    "license": "LGPL-3",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/server-env",
+    "maintainers": ["ivantodorovich"],
+    "depends": ["payment", "server_environment"],
+    "data": ["views/payment_acquirer.xml"],
+}
---
+++ payment_environment/readme/USAGE.rst
@@ -0,0 +1,2 @@
+Once configured, Odoo will read the payment acquirer values from the
+configuration file related to each environment defined in the main Odoo file.
