39beb9d0b6e2b31eaa9bf2679b9344a9999aeb18
Ivàn Todorovich <ivan.todorovich@gmail.com>
2022-03-17 12:54:58 -0300

    [ADD] mail_environment_google_gmail

---
+++ mail_environment_google_gmail/models/ir_mail_server.py
@@ -0,0 +1,20 @@
+# Copyright 2022 Camptocamp SA (https://www.camptocamp.com).
+# @author Iván Todorovich <ivan.todorovich@camptocamp.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class IrMailServer(models.Model):
+    _inherit = "ir.mail_server"
+
+    @property
+    def _server_env_fields(self):
+        base_fields = super()._server_env_fields
+        gmail_fields = {
+            "use_google_gmail_service": {},
+            "google_gmail_authorization_code": {},
+            "google_gmail_refresh_token": {},
+        }
+        gmail_fields.update(base_fields)
+        return gmail_fields
