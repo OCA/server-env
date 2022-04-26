29555be7b94928a9b2a6c609e8923cd7a0f603dc
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2022-04-26 08:58:20 +0200

    [FIX] auth_saml_environment: README

--- auth_saml_environment/__manifest__.py
+++ auth_saml_environment/__manifest__.py
@@ -2,9 +2,9 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    "name": "Auth SAML environement",
-    "summary": "Allows system administrator to authenticate with any account",
-    "version": "14.0.1.0.0",
+    "name": "Auth SAML environment",
+    "summary": "Configure SAML authentication providers through environment variables",
+    "version": "14.0.1.0.1",
     "category": "base",
     "author": "Camptocamp SA,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/server-env",
