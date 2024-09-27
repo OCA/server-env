ddbf505e9a6ee8c175a4478c208707748d8a36f5
Quentin Groulard <quentin.groulard@acsone.eu>
2024-09-27 17:08:04 +0200

    [MIG] server_environment_ir_config_parameter: Migration to 18.0

--- server_environment_ir_config_parameter/__manifest__.py
+++ server_environment_ir_config_parameter/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Server Environment Ir Config Parameter",
     "summary": """
         Override System Parameters from server environment file""",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/server-env",
