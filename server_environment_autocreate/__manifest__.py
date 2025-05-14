511ea29ffa9aa93d831ea2a8b52d8a7b693ac37a
Vincent Hatakeyama <vincent.hatakeyama@xcg-consulting.fr>
2025-05-14 16:25:56 +0200

    [MIG] server_environment_autocreate: Migration to 18.0

--- server_environment_autocreate/__manifest__.py
+++ server_environment_autocreate/__manifest__.py
@@ -4,11 +4,16 @@
 {
     "name": "Server Environment Autocreate",
     "summary": """Add ability to auto create records""",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "LGPL-3",
     "author": "XCG Consulting,Odoo Community Association (OCA)",
     "depends": ["server_environment"],
     "development_status": "Beta",
     "category": "Tools",
     "website": "https://github.com/OCA/server-env",
+    "external_dependencies": {
+        "python": [
+            "odoo-test-helper",
+        ],
+    },
 }
