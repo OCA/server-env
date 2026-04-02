c025687dbc8d6ee17f4ff95812ec5074d6ca6b74
Quentin Groulard <quentin.groulard@acsone.eu>
2026-04-02 15:23:54 +0200

    [FIX] mail_environment: Add migration script for field "smtp_authentication"
    To preserve the value of field "smtp_authentication" if we don't set it from
    the environment.
    Which may happen to anyone updating the sources of mail_environment for its project.

--- mail_environment/__manifest__.py
+++ mail_environment/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Mail configuration with server_environment",
-    "version": "19.0.1.1.0",
+    "version": "19.0.1.1.1",
     "category": "Tools",
     "summary": "Configure mail servers with server_environment_files",
     "author": "Camptocamp, Odoo Community Association (OCA)",
